edaa4bd8972506c15ef40dd2f322901cf7b244d2
0x4bd0 <abdoumjr@gmail.com>
2020-01-07 20:38:55 +0100

    getting started

---
+++ index.js
@@ -0,0 +1,11 @@
+let  generateArrayOfCharsFromAscii  = (from, to) => {
+    const n = []
+    for (let i = low; i <= high; i++) {
+      n.push(i)
+    }
+    return n
+  }
+
+const rsg = (length = 10,upperCase = false,lowerCase=true,specialChars=false,numbers=true) => {
+    
+}
