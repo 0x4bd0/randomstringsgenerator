804f290906360b1dbccff99d0cb1748b132383bf
0x4bd0 <abdoumjr@gmail.com>
2020-01-07 20:45:39 +0100

    allmost there

--- index.js
+++ index.js
@@ -7,5 +7,12 @@ let  generateArrayOfCharsFromAscii  = (from, to) => {
   }
 
 const rsg = (length = 10,upperCase = false,lowerCase=true,specialChars=false,numbers=true) => {
+    let genFrom = []
+    if(upperCase) genFrom = genFrom.concat(generateArrayOfCharsFromAscii(65, 90))
+    if(lowerCase) genFrom =genFrom.concat(generateArrayOfCharsFromAscii(97, 122))
+    if(numbers) genFrom = genFrom.concat(generateArrayOfCharsFromAscii(48, 57))
+    if(specialChars) genFrom = genFrom.concat(generateArrayOfCharsFromAscii(33, 47)).concat(generateArrayOfCharsFromAscii(58, 64)).concat(generateArrayOfCharsFromAscii(91, 96)).concat(generateArrayOfCharsFromAscii(123, 126))
+
+
     
 }
